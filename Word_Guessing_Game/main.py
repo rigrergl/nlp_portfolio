6bc99ee789380c0a87647612cda65960301a3a9c
rigrergl <rigrergl@gmail.com>
2023-02-17 19:32:57 -0600

    [Word Guessing Game] [Preprocess Raw Text] Tokenize text

--- Word_Guessing_Game/main.py
+++ Word_Guessing_Game/main.py
@@ -1,15 +1,11 @@
 import sys
 import os
 import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 
 
 def main():
-    #  Download tokenizer 'punkt'
-    if not os.path.exists(os.path.join(nltk.data.find('tokenizers'), 'punkt')):
-        nltk.download('punkt')
-    else:
-        print("Package punkt found, download skipped")
-
     # check args
     if len(sys.argv) < 2:
         print("Usage: python main.py [file_name]")
@@ -21,10 +17,23 @@ def main():
         text = f.read()
 
     # Calculate Lexical Diversity
-    tokens = nltk.word_tokenize(text)
-    tokens = [t.lower() for t in tokens]
+    tokens = nltk.word_tokenize(text.lower())
     token_set = set(tokens)
     print("Lexical Diversity: %.2f" % (len(token_set) / len(tokens)))
+
+    # preprocess raw text
+    tokens, nouns = preprocess_raw_text(text)
+
+
+def preprocess_raw_text(raw_text):
+    stop_words = nltk.corpus.stopwords.words('english')
+
+    # Tokenize text
+    tokens = nltk.word_tokenize(raw_text.lower())
+    tokens = [t for t in tokens if t.isalpha() and t not in stop_words and len(t) > 5]
+    print(len(tokens))
+
+    return None, None
 
 
 if __name__ == "__main__":
