ef7279eda756843d6f208838285468541e1c7e1b
rigrergl <rigrergl@gmail.com>
2023-03-02 15:33:18 -0600

    [ngrams] identify language

--- ngrams/program2.py
+++ ngrams/program2.py
@@ -1,10 +1,37 @@
 import pickle
 import pathlib
 
+from nltk import word_tokenize
+from nltk.util import ngrams
+
 
 def calc_lang_prob(line):
-    print(line)
-    return None, None, None
+    unigrams_test = word_tokenize(line)
+    bigrams_test = list(ngrams(unigrams_test, 2))
+
+    total_v_size = len(unigram_dict_en) + len(unigram_dict_fr) + len(unigram_dict_it)
+
+    p_laplace_en = 1
+    p_laplace_fr = 1
+    p_laplace_it = 1
+
+    for bigram in bigrams_test:
+        # Multiply by next probability in English
+        n_en = bigram_dict_en[bigram] if bigram in bigram_dict_en else 0
+        u_en = unigram_dict_en[bigram[0]] if bigram[0] in unigram_dict_en else 0
+        p_laplace_en = p_laplace_en * ((n_en + 1) / (u_en + total_v_size))
+
+        # Multiply by next probability in French
+        n_fr = bigram_dict_fr[bigram] if bigram in bigram_dict_fr else 0
+        u_fr = unigram_dict_fr[bigram[0]] if bigram[0] in unigram_dict_fr else 0
+        p_laplace_fr = p_laplace_fr * ((n_fr + 1) / (u_fr + total_v_size))
+
+        # Multiply by next probability in Italian
+        n_it = bigram_dict_it[bigram] if bigram in bigram_dict_it else 0
+        u_it = unigram_dict_it[bigram[0]] if bigram[0] in unigram_dict_it else 0
+        p_laplace_it = p_laplace_it * ((n_it + 1) / (u_it + total_v_size))
+
+    return p_laplace_en, p_laplace_fr, p_laplace_it
 
 
 if __name__ == '__main__':
@@ -26,3 +53,10 @@ if __name__ == '__main__':
 
     for test_line in test_text:
         en_prob, fr_prob, it_prob = calc_lang_prob(test_line)
+        if en_prob > fr_prob and en_prob > it_prob:
+            print('English')
+        elif fr_prob > it_prob:
+            print('French')
+        else:
+            print('Italian')
+
