21f9bb40b2f18d9eda8ec408859ca0d021c65dfa
rigrergl <rigrergl@gmail.com>
2023-03-02 15:05:08 -0600

    [ngrams] read in test file

--- ngrams/program2.py
+++ ngrams/program2.py
@@ -1,4 +1,11 @@
 import pickle
+import pathlib
+
+
+def calc_lang_prob(line):
+    print(line)
+    return None, None, None
+
 
 if __name__ == '__main__':
     # Get English dictionaries
@@ -12,3 +19,10 @@ if __name__ == '__main__':
     # Get Italian dictionaries
     bigram_dict_it = pickle.load(open('bigram_dict_it', 'rb'))
     unigram_dict_it = pickle.load(open('unigram_dict_it', 'rb'))
+
+    # Read in test file
+    with open(pathlib.Path.cwd().joinpath('data').joinpath('LangId.test'), encoding='utf8') as f:
+        test_text = f.read().splitlines()
+
+    for test_line in test_text:
+        en_prob, fr_prob, it_prob = calc_lang_prob(test_line)
