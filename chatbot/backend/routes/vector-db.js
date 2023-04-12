6a1f401f4b8e91eab7526fa74966a3952a9bf5cd
rigrergl <rigrergl@gmail.com>
2023-04-12 03:02:49 -0500

    [chatbot] query works

--- chatbot/backend/routes/vector-db.js
+++ chatbot/backend/routes/vector-db.js
@@ -20,11 +20,11 @@ router.post("/setup-db", async (req, res, next) => {
                 'description': 'Short summary of the chapter',
                 'dataType': ['string']
             },
-            {
-                'name': 'longSummary',
-                'description': 'Long summary of the chapter',
-                'dataType': ['string'],
-            },
+            // {
+            //     'name': 'longSummary',
+            //     'description': 'Long summary of the chapter',
+            //     'dataType': ['string'],
+            // },
             {
                 'name': 'chapterNumber',
                 'description': 'The number of the chapter',
@@ -44,7 +44,7 @@ router.post("/setup-db", async (req, res, next) => {
         .withClass(schemaConfig)
         .do();
 
-    res.status(200);
+    res.status(200).send('db successfully setup');
 })
 
 router.post("/write-data", async (req, res, next) => {
@@ -59,7 +59,7 @@ router.post("/write-data", async (req, res, next) => {
                 .withClassName('Chapter')
                 .withProperties({
                     shortSummary: obj.short_summary,
-                    longSummary: obj.long_summary,
+                    // longSummary: obj.long_summary,
                     chapterNumber: obj.id
                 })
                 .do()
@@ -73,24 +73,55 @@ router.post("/write-data", async (req, res, next) => {
     }
 })
 
-router.get("/get-all-objects", async (req, res, next) => {
-    client.graphql
-      .aggregate()
-      .withClassName("Chapter")
-      .do()
-      .then(res => {
-        console.log(JSON.stringify(res, null, 2))
-      })
-      .catch(err => {
-        console.error(JSON.stringify(err, null, 2))
-      });
+router.get("/get-object-count", async (req, res, next) => {
+    await client.graphql
+        .aggregate()
+        .withClassName("Chapter")
+        .withFields('meta { count }')
+        .do()
+        .then(res => {
+            console.log(JSON.stringify(res, null, 2))
+        })
+        .catch(err => {
+            console.error(JSON.stringify(err, null, 2))
+        });
 
     res.send(200)
 })
 
+router.get("/get-all-objects", async (req, res, next) => {
+    await client
+        .data
+        .getter()
+        .withClassName('Chapter')
+        .withLimit(2)
+        .do()
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.error(err)
+        });
+
+    res.send(200)
+})
+
+router.get("/query", async (req, res, next) => {
+    const testText = "luffy meets zoro";
+
+    const resText = await client.graphql.get()
+        .withClassName('Chapter')
+        .withFields(['shortSummary'])
+        .withNearText({concepts: [testText]})
+        .withLimit(1)
+        .do()
+    
+    console.log(resText.data.Get.Chapter[0].shortSummary)
+})
+
 router.get("/get-schema", async (req, res, next) => {
     const schemaRes = await client.schema.getter().do();
-
+    console.log(schemaRes)
     res.json({
         schema: schemaRes
     })
